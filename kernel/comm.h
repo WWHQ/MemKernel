7fb035227c1666409a03a77acc403894be4387a4
谓我何求 <54165654+WWHQ@users.noreply.github.com>
2026-06-08 11:44:14 +0800

    Update comm.h

--- kernel/comm.h
+++ kernel/comm.h
@@ -1,21 +1,21 @@
-struct CopyMemory
+typedef struct _COPY_MEMORY
 {
 	pid_t pid;
 	uintptr_t addr;
 	void *buffer;
 	size_t size;
-};
+} COPY_MEMORY, *PCOPY_MEMORY;
 
-struct ModuleBase
+typedef struct _MODULE_BASE
 {
 	pid_t pid;
 	char *name;
 	uintptr_t base;
-};
+} MODULE_BASE, *PMODULE_BASE;
 
-enum Operations
+enum OPERATIONS
 {
-	OP_INIT_KEY = 0x30000, // 未启用
+	OP_INIT_KEY = 0x3000,
 	OP_READ_MEM = 0x30001,
 	OP_WRITE_MEM = 0x30002,
 	OP_MODULE_BASE = 0x30003,
