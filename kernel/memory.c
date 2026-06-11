2d67dff10429a65820a990a53ce970ee0a040908
谓我何求 <54165654+WWHQ@users.noreply.github.com>
2026-06-11 09:34:21 +0800

    Update memory.c

--- kernel/memory.c
+++ kernel/memory.c
@@ -110,12 +110,10 @@ phys_addr_t translate_linear_address(struct mm_struct *mm, uintptr_t va)
 }
 #endif
 
-#ifndef ARCH_HAS_VALID_PHYS_ADDR_RANGE
 static inline int my_valid_phys_addr_range(phys_addr_t addr, size_t count)
 {
 	return addr + count <= __pa(high_memory);
 }
-#endif
 
 bool read_physical_address(phys_addr_t pa, void *buffer, size_t size)
 {
