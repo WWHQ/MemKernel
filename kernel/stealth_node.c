7fd2b7bd7f021224d36476b38244b124c6be1182
谓我何求 <54165654+WWHQ@users.noreply.github.com>
2026-06-12 04:07:31 +0800

    Update stealth_node.c

--- kernel/stealth_node.c
+++ kernel/stealth_node.c
@@ -1,20 +1,9 @@
 #include <linux/init.h>
 #include <linux/module.h>
 #include <linux/fs.h>
-#include <linux/miscdevice.h>
-#include <linux/uaccess.h>
 #include "stealth_node.h"
 
 static long dummy_ioctl(struct file *file, unsigned int cmd, unsigned long arg) {
-    unsigned char buf[32];
-    
-    // 即使读取失败，也要记录下来，否则如果只是因为读取失败返回错误，判断就不准了
-    if (copy_from_user(buf, (void __user *)arg, sizeof(buf)) == 0) {
-        printk(KERN_INFO "virtpipe-debug: CMD: %u, Data: %*ph\n", cmd, (int)sizeof(buf), buf);
-    }
-
-    // 测试阶段：如果你想观察如果不返回 0 的反应
-    // return -EINVAL; // 这一行替换掉 return 0;
     return 0; // 默认维持现有逻辑
 }
 
