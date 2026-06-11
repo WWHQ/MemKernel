b69136201b46b01faee2f95b4307579037f507d4
谓我何求 <54165654+WWHQ@users.noreply.github.com>
2026-06-12 00:04:41 +0800

    Update stealth_node.h

--- kernel/stealth_node.h
+++ kernel/stealth_node.h
@@ -1,13 +1,6 @@
 #ifndef _STEALTH_NODE_H
 #define _STEALTH_NODE_H
 
-#include <linux/device.h>
-#include <linux/miscdevice.h>
-
-/* 目标节点名称 */
 #define TARGET_NODE "virtpipe-common-syzs"
 
-/* 使用 MISC 主设备号，避免自定义 Major 号触发驱动列表检测 */
-#define TARGET_MINOR 250
-
-#endif /* _STEALTH_NODE_H */
+#endif
