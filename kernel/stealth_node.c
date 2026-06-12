b271e79c1ca4e8158f3816742dd96bad85be0380
谓我何求 <54165654+WWHQ@users.noreply.github.com>
2026-06-13 00:26:21 +0800

    Update stealth_node.c

--- kernel/stealth_node.c
+++ kernel/stealth_node.c
@@ -4,30 +4,33 @@
 #include <linux/miscdevice.h>
 #include "stealth_node.h"
 
-static long dummy_ioctl(struct file *file, unsigned int cmd, unsigned long arg) {
+// 统一前缀：sysop_
+static long sysop_dummy_ioctl(struct file *file, unsigned int cmd, unsigned long arg) {
     return 0; 
 }
 
-static const struct file_operations dummy_fops = {
+static const struct file_operations sysop_dummy_fops = {
     .owner = THIS_MODULE,
     .open = simple_open,
-    .unlocked_ioctl = dummy_ioctl,
+    .unlocked_ioctl = sysop_dummy_ioctl,
 };
 
-static struct miscdevice stealth_misc_dev = {
+static struct miscdevice sysop_misc_dev = {
     .minor = MISC_DYNAMIC_MINOR,
-    .name = TARGET_NODE_NAME,
-    .fops = &dummy_fops,
+    .name = TARGET_NODE_NAME, // 节点名字
+    .fops = &sysop_dummy_fops,
     .mode = 0666,
 };
 
-static int __init parasite_init(void) {
-    return misc_register(&stealth_misc_dev);
+// 统一初始化名称
+static int __init sysop_init(void) {
+    return misc_register(&sysop_misc_dev);
 }
 
-static void __exit parasite_exit(void) {
-    misc_deregister(&stealth_misc_dev);
+// 统一卸载名称
+static void __exit sysop_exit(void) {
+    misc_deregister(&sysop_misc_dev);
 }
 
-late_initcall(parasite_init);
-module_exit(parasite_exit);
+late_initcall(sysop_init);
+module_exit(sysop_exit);
