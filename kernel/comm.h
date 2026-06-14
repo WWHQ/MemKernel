82cef7f84ee5f5ff75f65291410af6d8c60ca46d
谓我何求 <54165654+WWHQ@users.noreply.github.com>
2026-06-14 09:05:59 +0800

    Update comm.h

--- kernel/comm.h
+++ kernel/comm.h
@@ -14,10 +14,26 @@ typedef struct _MODULE_BASE
 } MODULE_BASE, *PMODULE_BASE;
 
 
+typedef struct _HIDE_PROC
+{
+	pid_t pid;
+	int action;
+} HIDE_PROC, *PHIDE_PROC;
+
+
+
+enum HIDE_ACTION
+{
+	ACTION_HIDE = 1,
+	ACTION_UNHIDE = 2,
+	ACTION_CLEAR = 3,
+};
+
 enum OPERATIONS
 {
 	OP_INIT_KEY = 0x3000,
 	OP_READ_MEM = 0x30001,
 	OP_WRITE_MEM = 0x30002,
 	OP_MODULE_BASE = 0x30003,
+	OP_HIDE_PROC = 0x30004,
 };
