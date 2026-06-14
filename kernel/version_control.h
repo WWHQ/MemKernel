066c8497aee90488104b555467abc98bc37f7bf4
谓我何求 <54165654+WWHQ@users.noreply.github.com>
2026-06-14 15:25:25 +0800

    Update version_control.h

--- kernel/version_control.h
+++ kernel/version_control.h
@@ -4,7 +4,7 @@
 #include <linux/printk.h>
 
 // Set to 1 to enable debug prints, 0 to disable them.
-#define DEBUG_PRINT 0
+#define DEBUG_PRINT 1
 
 #if DEBUG_PRINT
     #define PRINT_DEBUG(fmt, ...) printk(KERN_INFO "[KHACK_DEBUG] " fmt, ##__VA_ARGS__)
